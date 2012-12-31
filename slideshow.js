1d6071de8320647a6b64779a20e5a890fadbe7a7
Brian Berlin <bri@nberl.in>
2012-12-31 13:07:25 -0600

    removed nivo and added custom jquery slider

--- slideshow.js
+++ slideshow.js
@@ -1,24 +1,72 @@
 (function ($) {
-  Drupal.behaviors.slideshow = {
-    attach: function (context, settings) {
-      // Initialize the slider
-      $('#block-slideshow-slideshow div.content ul').nivoSlider({
-        effect: 'random', // Specify sets like: 'fold,fade,sliceDown'
-        slices: 15, // For slice animations
-        boxCols: 8, // For box animations
-        boxRows: 4, // For box animations
-        animSpeed: 500, // Slide transition speed
-        pauseTime: 3000, // How long each slide will show
-        startSlide: 0, // Set starting Slide (0 index)
-        directionNav: true, // Next & Prev navigation
-        controlNav: true, // 1,2,3... navigation
-        controlNavThumbs: false, // Use thumbnails for Control Nav
-        pauseOnHover: true, // Stop animation while hovering
-        manualAdvance: false, // Force manual transitions
-        prevText: 'Prev', // Prev directionNav text
-        nextText: 'Next', // Next directionNav text
-        randomStart: false, // Start on a random slide
+
+
+Drupal.behaviors.slideshow = {
+	attach: function (context, settings) {
+		$("#slideshow").gallerify();
+	}
+}
+
+
+jQuery.fn.gallerify = function() {
+    return this.each(function(){
+
+      id = Math.floor(Math.random()*100000);
+
+      var items = $('li ',this);
+
+      $(this).css({
+      	'position':'relative',
+      	'width':$(items[0]).width(),
+      	'height':$(items[0]).height(),
+      })
+
+      items.css({
+      	'opacity':0,
+      	"z-index":"1",
+      	'position':'absolute',
       });
-    }
-  };
+
+      $(items[0]).css({
+      	'opacity':1,
+      	"z-index":"3"
+      });
+
+      if (items.length > 1) {
+        $(this).after('<div class="gallery_changer"><ul id="g'+id+'"></ul></div>');
+        var changer = $('#g'+id);
+        items.each(function(){
+          var numberLink = (items.index(this)+1).toString();
+          if (numberLink.length == 1) numberLink = '0' + numberLink;
+          $('<li><a href="#">'+numberLink+'</a></li>').click(showImage).appendTo(changer);
+        });
+        $('li:first',changer).addClass('first current');
+        $('li:last',changer).addClass('last');
+
+	    $('.gallery_changer').css({
+	    	'position':'relative',
+	    	'z-index':10
+	    });
+      }
+
+
+      function showImage() {
+
+        var $clicked = $(items[$(this).index()]);
+        var $current = $(items[$(this).parent().children('li.current').index()]);
+
+        $clicked.parent().animate({'width':$clicked.width(), 'height': $clicked.height()});
+        $current.css({"z-index":"2"});
+        $clicked.css({"z-index":"3"}).animate({opacity:1},400, function(){
+          $current.css({"opacity":"0","z-index":"1"});
+        });
+
+        $(this).addClass('current').siblings().removeClass('current');
+
+        return false;
+      }
+  });
+}
+
+
 }(jQuery));
