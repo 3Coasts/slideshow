45017f9a171489055c7f9da614a9aa3faf5f5ff6
Brian Berlin <bri@nberl.in>
2013-01-04 14:39:34 -0600

    Changes based on needs for AMPension

--- slideshow.js
+++ slideshow.js
@@ -11,29 +11,21 @@ Drupal.behaviors.slideshow = {
 jQuery.fn.gallerify = function() {
     return this.each(function(){
 
-      id = Math.floor(Math.random()*100000);
-
+      var id = Math.floor(Math.random()*100000);
       var items = $('li ',this);
+      var obj = this;
+      var ratio;
 
-      $(this).css({
-      	'position':'relative',
-      	'width':$(items[0]).width(),
-      	'height':$(items[0]).height(),
-      })
+      items.css({ 'display':'none' });
 
-      items.css({
-      	'opacity':0,
-      	"z-index":"1",
-      	'position':'absolute',
+      items.find("img").parent().css({
+        'float':'left',
       });
 
-      $(items[0]).css({
-      	'opacity':1,
-      	"z-index":"3"
-      });
+      $(items[0]).css({ 'display':'block' });
 
       if (items.length > 1) {
-        $(this).after('<div class="gallery_changer"><ul id="g'+id+'"></ul></div>');
+        $(this).before('<div class="gallery_changer"><ul id="g'+id+'"></ul></div>');
         var changer = $('#g'+id);
         items.each(function(){
           var numberLink = (items.index(this)+1).toString();
@@ -43,28 +35,42 @@ jQuery.fn.gallerify = function() {
         $('li:first',changer).addClass('first current');
         $('li:last',changer).addClass('last');
 
-	    $('.gallery_changer').css({
-	    	'position':'relative',
-	    	'z-index':10
-	    });
       }
 
+      var auto;
+      var current = 0;
+      var max = $(items).length -1;
+
+      autotoggle('on');
+      $(obj).parents(".block").hover(function(){ autotoggle('off'); }, function(){ autotoggle('on'); });
+
+      function autotoggle(state){
+        if(state == 'on'){
+          console.log('go');
+          clearInterval(auto);
+          auto = setInterval(function(){
+            if(current < max){ current++; }else{ current=0; }
+            $(obj).parent().find('.gallery_changer li')[current].click();
+          },5000);
+        }else{
+          clearInterval(auto);
+          console.log('stop');
+        }
+      }
 
       function showImage() {
 
         var $clicked = $(items[$(this).index()]);
         var $current = $(items[$(this).parent().children('li.current').index()]);
 
-        $clicked.parent().animate({'width':$clicked.width(), 'height': $clicked.height()});
-        $current.css({"z-index":"2"});
-        $clicked.css({"z-index":"3"}).animate({opacity:1},400, function(){
-          $current.css({"opacity":"0","z-index":"1"});
-        });
-
-        $(this).addClass('current').siblings().removeClass('current');
-
+        if($clicked.index() != $current.index()){
+          $current.fadeOut();
+          $clicked.fadeIn();
+          $(this).addClass('current').siblings().removeClass('current');
+        }
         return false;
       }
+
   });
 }
 
